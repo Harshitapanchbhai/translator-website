5c5580402cf3c13e9d1b99d9f39b67e7f3e99307
SadhvikaIndala <86656428+SadhvikaIndala@users.noreply.github.com>
2021-06-30 18:05:11 +0530

    Update main (7).py

--- main (7).py
+++ main (7).py
@@ -6,7 +6,7 @@ root = Tk()
 root.geometry('500x400')
 root.title('Translator')
 root.resizable(False,False)
-root.configure(bg='Orange')
+root.configure(bg='lightblue')
 lan_dict = {'afrikaans': 'af', 'albanian': 'sq', 'amharic': 'am', 'arabic': 'ar', 'armenian': 'hy', 'azerbaijani': 'az', 'basque': 'eu', 'belarusian': 'be', 'bengali': 'bn', 'bosnian': 'bs', 'bulgarian': 'bg', 'catalan': 'ca', 'cebuano': 'ceb', 'chichewa': 'ny', 'chinese (simplified)': 'zh-cn', 'chinese (traditional)': 'zh-tw', 'corsican': 'co', 'croatian': 'hr', 'czech': 'cs', 'danish': 'da', 'dutch': 'nl', 'english': 'en', 'esperanto': 'eo', 'estonian': 'et', 'filipino': 'tl', 'finnish': 'fi', 'french': 'fr', 'frisian': 'fy', 'galician': 'gl', 'georgian': 'ka', 'german': 'de', 'greek': 'el', 'gujarati': 'gu', 'haitian creole': 'ht', 'hausa': 'ha', 'hawaiian': 'haw', 'hebrew': 'he', 'hindi': 'hi', 'hmong': 'hmn', 'hungarian': 'hu', 'icelandic': 'is', 'igbo': 'ig', 'indonesian': 'id', 'irish': 'ga', 'italian': 'it', 'japanese': 'ja', 'javanese': 'jw', 'kannada': 'kn', 'kazakh': 'kk', 'khmer': 'km', 'korean': 'ko', 'kurdish (kurmanji)': 'ku', 'kyrgyz': 'ky', 'lao': 'lo', 'latin': 'la', 'latvian': 'lv', 'lithuanian': 'lt', 'luxembourgish': 'lb', 'macedonian': 'mk', 'malagasy': 'mg', 'malay': 'ms', 'malayalam': 'ml', 'maltese': 'mt', 'maori': 'mi', 'marathi': 'mr', 'mongolian': 'mn', 'myanmar (burmese)': 'my', 'nepali': 'ne', 'norwegian': 'no', 'odia': 'or', 'pashto': 'ps', 'persian': 'fa', 'polish': 'pl', 'portuguese': 'pt', 'punjabi': 'pa', 'romanian': 'ro', 'russian': 'ru', 'samoan': 'sm', 'scots gaelic': 'gd', 'serbian': 'sr', 'sesotho': 'st', 'shona': 'sn', 'sindhi': 'sd', 'sinhala': 'si', 'slovak': 'sk', 'slovenian': 'sl', 'somali': 'so', 'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tajik': 'tg', 'tamil': 'ta', 'telugu': 'te', 'thai': 'th', 'turkish': 'tr', 'ukrainian': 'uk', 'urdu': 'ur', 'uyghur': 'ug', 'uzbek': 'uz', 'vietnamese': 'vi', 'welsh': 'cy', 'xhosa': 'xh', 'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu'}
 def tt():
     try:
@@ -62,17 +62,17 @@ entry2 = Entry(root,width=30,textvariable=varname2,font=('times',15,'italic bold
 entry2.place(x=150,y=100)
 
 ########################################################################## Labels
-label1 = Label(root,text='Enter Words : ',font=('times',15,'italic bold'),bg='Orange')
+label1 = Label(root,text='Enter Words : ',font=('times',15,'italic bold'),bg='lightblue')
 label1.place(x=5,y=40)
 
-label2 = Label(root,text='Translated : ',font=('times',15,'italic bold'),bg='Orange')
+label2 = Label(root,text='Translated : ',font=('times',15,'italic bold'),bg='lightblue')
 label2.place(x=5,y=100)
 
-label3 = Label(root,text=' ',font=('times',15,'italic bold'),bg='Orange')
+label3 = Label(root,text=' ',font=('times',15,'italic bold'),bg='lightblue')
 label3.place(x=10,y=250)
 
 ############################################################################ Buttons
-btn1 = Button(root,text='Click',bd=10,bg='white',activebackground='brown',width=10,font=('times',15,'italic bold'),
+btn1 = Button(root,text='Translate',bd=10,bg='white',activebackground='brown',width=10,font=('times',15,'italic bold'),
               command=tt)
 btn1.place(x=70,y=170)
 
